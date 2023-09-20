61c382e898b8ae82e8c60407e2afbb6da90f6422
akaganov123 <akaganov25@riverdale.k12.or.us>
2023-09-20 10:56:14 -0700

    aniamtion code basics
    title and reorganization

--- Bjorn-Ari-Game/Assets/AI.cs
+++ Bjorn-Ari-Game/Assets/AI.cs
@@ -6,6 +6,7 @@ using UnityEngine.AI;
 public class AI : MonoBehaviour
 {
 
+
     public bool playerFound = false;
     private NavMeshAgent[] navAgents;
 
@@ -17,9 +18,22 @@ public class AI : MonoBehaviour
 
     void Update()
     {
+        RaycastHit hit;
+
+        Vector3 p1 = transform.position;
+        float distanceToObstacle = 0;
+
+        // Cast a sphere wrapping character controller 10 meters forward
+        // to see if it is about to hit anything.
+        if (Physics.SphereCast(p1, 1, transform.forward, out hit, 10))
+        {
+            distanceToObstacle = hit.distance;
+        }
+        
         if(playerFound)
         {
             agent.destination = GameObject.FindWithTag("Player").transform.position;
         }
+
     }
 }
--- Bjorn-Ari-Game/Assets/Scripts/AnimationPlayerScript.cs
+++ Bjorn-Ari-Game/Assets/Scripts/AnimationPlayerScript.cs
@@ -18,23 +18,80 @@ public class AnimationPlayerScript : MonoBehaviour
         bool isRunning = animator.GetBool("isRunning");
         bool runPressed = Input.GetKey("left shift");
         bool isWalking = animator.GetBool("isWalking");
-        bool forwardPressed = Input.GetKey("w");
-        if(isWalking && forwardPressed)
+        bool forwardPressed = Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("d");
+        bool isJumping = animator.GetBool("isJumping");
+        bool upPressed = Input.GetKey("space");
+        bool isCrouching = animator.GetBool("isCrouching");
+        bool downPressed = Input.GetKey("left control");
+        bool isCwalking = animator.GetBool("isCwalking");
+        bool cforwardPressed = Input.GetKey("left control") && Input.GetKey("w");
+        bool isBwalking = animator.GetBool("isBwalking");
+        bool backwardsPressed = Input.GetKey("s");
+
+        if (forwardPressed)
         {
             animator.SetBool("isWalking", true);
         }
-        if(isWalking && !forwardPressed)
+        if (!forwardPressed)
         {
-            animator.SetBool("isWalking", false);
+             animator.SetBool("isWalking", false);
         }
-
-        if(!isRunning && (forwardPressed && runPressed))
+        if (runPressed && forwardPressed)
         {
             animator.SetBool("isRunning", true);
         }
-        if(!isRunning && (!forwardPressed || !runPressed))
+        if (!runPressed || !forwardPressed)
         {
             animator.SetBool("isRunning", false);
         }
+        if (upPressed)
+        {
+            animator.SetBool("isJumping", true);
+        }
+        if (!upPressed)
+        {
+            animator.SetBool("isJumping", false);
+        }
+        if (downPressed)
+        {
+            animator.SetBool("isCrouching", true);
+        }
+        if (!downPressed)
+        {
+            animator.SetBool("isCrouching", false);
+        }
+        if (cforwardPressed)
+        {
+            animator.SetBool("isCwalking", true);
+        }
+        if (!cforwardPressed)
+        {
+            animator.SetBool("isCwalking", false);
+        }
+        if (backwardsPressed)
+        {
+            animator.SetBool("isBwalking", true);
+        }
+        if (!backwardsPressed)
+        {
+            animator.SetBool("isBwalking", false);
+        }
+        // if(isWalking && forwardPressed)
+        // {
+        //     animator.SetBool("isWalking", true);
+        // }
+        // if(isWalking && !forwardPressed)
+        // {
+        //     animator.SetBool("isWalking", false);
+        // }
+
+        // if(!isRunning && (forwardPressed && runPressed))
+        // {
+        //     animator.SetBool("isRunning", true);
+        // }
+        // if(!isRunning && (!forwardPressed || !runPressed))
+        // {
+        //     animator.SetBool("isRunning", false);
+        // }
     }
 }
