8cc4859d9081886c9e548809fcec56398a7eb5ab
akaganov123 <akaganov25@riverdale.k12.or.us>
2023-10-05 11:41:32 -0700

    all my work now
    work now

--- Bjorn-Ari-Game/Assets/Scripts/AnimationPlayerScript.cs
+++ Bjorn-Ari-Game/Assets/Scripts/AnimationPlayerScript.cs
@@ -22,7 +22,7 @@ public class AnimationPlayerScript : MonoBehaviour
         bool isJumping = animator.GetBool("isJumping");
         bool upPressed = Input.GetKey("space");
         bool isCrouching = animator.GetBool("isCrouching");
-        bool downPressed = Input.GetKey(KeyCode.LeftControl);
+        bool downPressed = Input.GetKey(KeyCode.LeftControl) && !(Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d"));;
         bool isCwalking = animator.GetBool("isCwalking");
         bool cforwardPressed = Input.GetKey(KeyCode.LeftControl) && (Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d"));
         bool isBwalking = animator.GetBool("isBwalking");
