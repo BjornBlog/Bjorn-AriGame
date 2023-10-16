8860f871d03fa55eb882d6c96d12c5ee77578d5b
BjornBlog <broberts25@riverdale.k12.or.us>
2023-10-16 12:30:02 -0700

    MErge

---
+++ Bjorn-Ari-Game/Assets/Scripts/PointsScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointsScript : MonoBehaviour
+{
+    int points = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    int CaluculateScore()
+    {
+
+    }
+}
