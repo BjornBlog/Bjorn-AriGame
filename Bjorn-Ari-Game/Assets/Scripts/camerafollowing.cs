37d6a00644903d1b9e153e688610982c7926d98b
akaganov123 <akaganov25@riverdale.k12.or.us>
2023-10-19 12:23:27 -0700

    debugging
    Camera

--- Bjorn-Ari-Game/Assets/Scripts/camerafollowing.cs
+++ Bjorn-Ari-Game/Assets/Scripts/camerafollowing.cs
@@ -2,26 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class camerafollowing : MonoBehaviour
+public class CameraFollowing : MonoBehaviour
 {
-    public Transform player;
-    public float mouseSensitivity = 2f;
-    float cameraVerticalRotation = 0f;
-    bool lockedCurser = true;
+    public GameObject player;
+    // Start is called before the first frame update
     void Start()
     {
-       Cursor.visible = false;
-       Cursor.lockState = CursorLockMode.Locked;
+        player = GameObject.Find("character");
     }
 
+    // Update is called once per frame
     void Update()
     {
-        float InputX = Input.GetAxis("Mouse X") * mouseSensitivity;
-        float InputY = Input.GetAxis("Mouse Y") * mouseSensitivity;
-
-        cameraVerticalRotation -= InputY;
-        cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, -90f, 40f);
-        transform.localEulerAngles = Vector3.right * cameraVerticalRotation;
-        // player.Rotate(Vector3.up * inputX;)
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 5, player.transform.position.z - 10);
     }
 }
