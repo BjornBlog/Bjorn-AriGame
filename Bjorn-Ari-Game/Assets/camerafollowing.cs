df2a343b7d0f64d2980ced8944e295057a5c7bd9
akaganov123 <akaganov25@riverdale.k12.or.us>
2023-10-24 12:30:21 -0700

    before merge
    ..

---
+++ Bjorn-Ari-Game/Assets/camerafollowing.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerafollowing : MonoBehaviour
+{
+    public Transform player;
+    public float mouseSensitivity = 2f;
+    float cameraVerticalRotation = 0f;
+    bool lockedCurser = true;
+    void Start()
+    {
+       Cursor.visible = false;
+       Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float InputX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float InputY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        cameraVerticalRotation -= InputY;
+        cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, -90f, 66f);
+        transform.localEulerAngles = Vector3.right * cameraVerticalRotation;
+        // player.Rotate(Vector3.up * inputX;)
+    }
+}
